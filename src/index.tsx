159d8c9c83946efdbea5814b75dc67b04ea5f39e
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-22 16:02:29 +0100

    highlight error

--- src/index.tsx
+++ src/index.tsx
@@ -55,8 +55,15 @@ const App = () => {
     <div id="root"></div>
     <script>
       window.addEventListener('message', (event) => { 
-        eval(event.data)
-      }, false)
+        try {
+          eval(event.data)
+        } catch (err) {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err)
+        }
+        
+      }, false);
     </script>
     </body>
     </html>
