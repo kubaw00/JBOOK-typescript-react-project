47b8dec1c382b49165529818117586e1e51e1048
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-23 18:15:01 +0100

    add prettier to our editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -16,23 +18,25 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
   };
 
   return (
-    <MonacoEditor
-      editorDidMount={editorDidMount}
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      value={initialValue}
-      theme='dark'
-      language='javascript'
-      height='500px'
-    />
+    <div>
+      <MonacoEditor
+        editorDidMount={editorDidMount}
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        value={initialValue}
+        theme='dark'
+        language='javascript'
+        height='500px'
+      />
+    </div>
   );
 };
 
