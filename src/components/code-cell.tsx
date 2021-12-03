916f36f009ee81db78d50203a941bc9a62317c6a
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-03 01:31:45 +0100

    add an execution timeout

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,16 +19,6 @@ const CodeCell = () => {
     };
   }, [input]);
 
-  const onClick = async () => {
-    // const res = await ref.current.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015',
-    // })
-    // setCode(res.code);
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
