d5d73bd361e8465ed1528c2ba401e8d7605671ad
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-04 04:11:51 +0100

    conditional toggling

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,15 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
       setEditing(false);
     };
 
     document.addEventListener('click', listener, { capture: true });
+
     return () => {
       document.removeEventListener('click', listener, { capture: true });
     };
@@ -17,7 +27,7 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div>
+      <div ref={ref}>
         <MDEditor />
       </div>
     );
