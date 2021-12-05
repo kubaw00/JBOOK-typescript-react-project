4f0873ac6f79469a8eaa81a15053e062ebd3374d
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-06 00:31:50 +0100

    simple updates with Immer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,4 +1,5 @@
 import { ActionType } from '../action-types';
+import produce from 'immer';
 import { Action } from '../actions';
 import { Cell } from '../cell';
 
@@ -18,20 +19,12 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: { ...state.data[id], content },
-        },
-      };
+      state.data[id].content = content;
+      return;
     case ActionType.DELETE_CELL:
       return state;
     case ActionType.MOVE_CELL:
@@ -39,6 +32,6 @@ const reducer = (
     case ActionType.INSERT_CELL_BEFORE:
       return state;
   }
-};
+});
 
 export default reducer;
