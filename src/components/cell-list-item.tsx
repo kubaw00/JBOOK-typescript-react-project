2487b78f86c980634af2c2d3eecdff66d56042cc
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-06 16:26:07 +0100

    create cell-list and cell-list-item

---
+++ src/components/cell-list-item.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CellListItem = () => {
+  return <div>Cell List Item</div>;
+};
+
+export default CellListItem;
---
+++ src/components/cell-list.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CellList = () => {
+  return <div>Cell List</div>;
+};
+
+export default CellList;
