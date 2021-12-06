96aa9d02b7b3fe585d8cddcacf8d717bf8440b94
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-06 16:41:30 +0100

    create a typed selector

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
 
 const CellList = () => {
+  useTypedSelector((state) => state);
+
   return <div>Cell List</div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
-
 import CellList from './components/cell-list';
 
 const App = () => {
