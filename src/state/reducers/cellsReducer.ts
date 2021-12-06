3de8d6e06c30a8254b118c141f7dc9f223835964
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-06 01:27:12 +0100

    remove cell

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -25,8 +25,12 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { id, content } = action.payload;
       state.data[id].content = content;
       return;
+
     case ActionType.DELETE_CELL:
-      return state;
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      return;
+
     case ActionType.MOVE_CELL:
       return state;
     case ActionType.INSERT_CELL_BEFORE:
