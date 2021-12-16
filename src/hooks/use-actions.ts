8f5e7f523ffe69eaf21daae7064d8e54544b7a99
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-16 18:32:05 +0100

    soleve a small waring with useMemo

--- src/hooks/use-actions.ts
+++ src/hooks/use-actions.ts
@@ -1,9 +1,12 @@
 import { actionCreators } from '../state';
+import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 export const useActions = () => {
   const dispatch = useDispatch();
 
-  return bindActionCreators(actionCreators, dispatch);
+  return useMemo(() => {
+    return bindActionCreators(actionCreators, dispatch);
+  }, [dispatch]);
 };
