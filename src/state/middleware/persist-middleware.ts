b60b12639c5daa18681a314543058fffd531ad5c
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-29 00:05:51 +0100

    debouncing save logic

--- src/state/middleware/persist-middleware.ts
+++ src/state/middleware/persist-middleware.ts
@@ -4,26 +4,34 @@ import { ActionType } from '../action-types';
 import { saveCells } from '../action-creators';
 import { RootState } from '..';
 
-export const persistMiddleware =
-  ({
-    dispatch,
-    getState,
-  }: {
-    dispatch: Dispatch<Action>;
-    getState: () => RootState;
-  }) =>
-  (next: (action: Action) => void) =>
-  (action: Action) => {
-    next(action);
+export const persistMiddleware = ({
+  dispatch,
+  getState,
+}: {
+  dispatch: Dispatch<Action>;
+  getState: () => RootState;
+}) => {
+  let timer: any;
+  return (next: (action: Action) => void) => {
+    return (action: Action) => {
+      next(action);
 
-    if (
-      [
-        ActionType.MOVE_CELL,
-        ActionType.INSERT_CELL_AFTER,
-        ActionType.DELETE_CELL,
-        ActionType.UPDATE_CELL,
-      ].includes(action.type)
-    ) {
-      saveCells()(dispatch, getState);
-    }
+      if (
+        [
+          ActionType.MOVE_CELL,
+          ActionType.INSERT_CELL_AFTER,
+          ActionType.DELETE_CELL,
+          ActionType.UPDATE_CELL,
+        ].includes(action.type)
+      ) {
+        if (timer) {
+          clearTimeout(timer);
+        }
+
+        timer = setTimeout(() => {
+          saveCells()(dispatch, getState);
+        }, 250);
+      }
+    };
   };
+};
