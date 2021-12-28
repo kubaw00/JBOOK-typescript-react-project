6e5ba638c0f1b1c7a24df8ed10d18654b9a5038a
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-28 20:54:04 +0100

    detect when to save

--- src/state/middleware/persist-middleware.ts
+++ src/state/middleware/persist-middleware.ts
@@ -2,10 +2,28 @@ import { Dispatch } from 'redux';
 import { Action } from '../actions';
 import { ActionType } from '../action-types';
 import { saveCells } from '../action-creators';
+import { RootState } from '..';
 
 export const persistMiddleware =
-  ({ dispatch }: { dispatch: Dispatch<Action> }) =>
+  ({
+    dispatch,
+    getState,
+  }: {
+    dispatch: Dispatch<Action>;
+    getState: () => RootState;
+  }) =>
   (next: (action: Action) => void) =>
   (action: Action) => {
     next(action);
+
+    if (
+      [
+        ActionType.MOVE_CELL,
+        ActionType.INSERT_CELL_AFTER,
+        ActionType.DELETE_CELL,
+        ActionType.UPDATE_CELL,
+      ].includes(action.type)
+    ) {
+      saveCells()(dispatch, getState);
+    }
   };
